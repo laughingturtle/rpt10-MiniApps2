e37cd5d198d46f795ebc339dc0a026a5d171b5e5
laughingturtle <monkeydo@Monkeys-MacBook-Pro.local>
2019-04-01 23:59:28 -0700

    basic minesweeper board setup

---
+++ challenge_4/app/src/components/board.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Square from './square.jsx';
+
+var Board = () => (
+
+  <div className="board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+
+)
+
+export default Board;
---
+++ challenge_4/app/src/components/square.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+var Square = () => (
+    <div className="square" id=""></div>
+)
+
+export default Square;
