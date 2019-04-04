fd161127597448891e8fa596119daa81cda035d8
laughingturtle <monkeydo@Monkeys-MacBook-Pro.local>
2019-04-04 15:55:46 -0700

    update

--- challenge_4/app/src/components/square.jsx
+++ challenge_4/app/src/components/square.jsx
@@ -1,9 +1,6 @@
 // this is a class component as opposed to a functional component,
 import React from 'react'
 
-
-var switchClass;
-
 class Square extends React.Component {
  // eslint-disable-next-line
   constructor(props){
@@ -16,34 +13,110 @@ class Square extends React.Component {
   this.display = this.display.bind(this);
   this.endGame = this.endGame.bind(this);
   this.clickHandler = this.clickHandler.bind(this);
+  this.findConnnectedSquares = this.findConnnectedSquares.bind(this);
  }
 
-  clickHandler(e){
+  clickHandler(e) {
     this.setState({isClicked : !this.state.isClicked});
-    this.props.revealClicked(e.target.id);
+
+    this.findConnnectedSquares(e.target.id);
+    //this.props.revealClicked(e.target.id);
     if (this.props.score === 8){
       /* end game */ // boom
       this.endGame()
     }
   }
 
-  display(){
+  findConnnectedSquares(hit) {
+    console.log('actual id: ', hit )
+    let id = hit.substr(1);
+   // console.log('id: ', id);
+    let a = parseInt(id.charAt(0));
+    let b = parseInt(id.charAt(1));
+    // console.log('a: ', a);
+    // console.log('b: ', b);
+    let board = this.props.gameboard;
+
+    function checkEightSquaresAround(a,b){
+     console.log('a: ', a);
+     console.log('b: ', b);
+      // check left
+      if(board[a]!== undefined && board[a][b -1] !== undefined){
+        if(board[a][b] !== 8 && board[a][b -1] !== 8){
+         // console.log('thus target this: ', '_' + a + '' + (b -1));
+           document.getElementById('_' + a + '' + (b -1)).click();
+          if(board[a][b] < 1 && board[a][b -1] < 1){
+            console.log('** we\'re in the recursion **');
+            console.log('current board value: ', board[a][b]);
+            console.log('board value one to the left : ', board[a][b -1]);
+            console.log('a:', a, 'b:', b)
+          //  checkEightSquaresAround(a,b -1);
+            console.log('** we\'re out of the recursion **');
+          }
+        }
+      }
+      // check top left
+      // if(board[a -1]!== undefined && board[a -1][b -1] !== undefined){
+      //   if(board[a][b] !== 8 && board[a-1][b -1] !== 8){
+      //     console.log('thus target this: ', '_' + (a -1) + '' + (b -1));
+      //     // /* what here?*/ ('_00').click(); // target the click event of this id
+      //   }
+      // }
+      // // check top
+      // if(board[a -1]!== undefined && board[a -1][b] !== undefined){
+      //   if(board[a][b] !== 8 && board[a-1][b] !== 8){
+      //     console.log('thus target this: ', '_' + (a -1) + '' + (b));
+      //     // /* what here?*/ ('_00').click(); // target the click event of this id
+      //   }
+      // }
+      // // check top right
+      // if(board[a -1]!== undefined && board[a -1][b +1] !== undefined){
+      //   if(board[a][b] !== 8 && board[a-1][b +1] !== 8){
+      //     console.log('thus target this: ', '_' + (a -1) + '' + (b +1));
+      //     // /* what here?*/ ('_00').click(); // target the click event of this id
+      //   }
+      // }
+      // // check right
+      // if(board[a]!== undefined && board[a][b +1] !== undefined){
+      //   if(board[a][b] !== 8 && board[a][b +1] !== 8){
+      //     console.log('thus target this: ', '_' + a + '' + (b +1));
+      //     // /* what here?*/ ('_00').click(); // target the click event of this id
+      //   }
+      // }
+      // // check bottom right
+      // if(board[a +1]!== undefined && board[a +1][b +1] !== undefined){
+      //   if(board[a][b] !== 8 && board[a+1][b +1] !== 8){
+      //     console.log('thus target this: ', '_' + (a +1) + '' + (b +1));
+      //     // /* what here?*/ ('_00').click(); // target the click event of this id
+      //   }
+      // }
+      // // check bottom
+      // if(board[a +1]!== undefined && board[a +1][b] !== undefined){
+      //   if(board[a][b] !== 8 && board[a+1][b] !== 8){
+      //     console.log('thus target this: ', '_' + (a +1) + '' + b);
+      //     // /* what here?*/ ('_00').click(); // target the click event of this id
+      //   }
+      // }
+      // // check bottom left
+      // if(board[a +1]!== undefined && board[a +1][b -1] !== undefined){
+      //   if(board[a][b] !== 8 && board[a+1][b -1] !== 8){
+      //     console.log('thus target this: ', '_' + (a +1) + '' + (b -1));
+      //     // /* what here?*/ ('_00').click(); // target the click event of this id
+      //   }
+      // }
+    }
+    checkEightSquaresAround(a,b);
+  }
+
+  display() {
    // console.log('gameboard: ', this.props.gameboard)
   /*
    if this.props.score = 1, 2, 3 different colors, if 0 don't show ,
    if 8 show background image. Change {this.props.score}
    in the render to the name of this function.
   */
-    if(this.props.score !== 0){
+    if(this.props.score !== 0 && this.props.score !== 8){
       return this.props.score;
-    }
-    if(this.props.score === 0){
-      return;
-    }
-    if (this.props.score === 8){
-      /* end game */ // boom
-      //this.endGame()
-      return;
     }
   }
 
@@ -54,6 +127,7 @@ class Square extends React.Component {
   }
 
   render() {
+    var switchClass;
     /* change css classes based on the the number in the square on the board and show if clicked */
     if (this.props.score === 8){
       switchClass = this.state.isClicked ? 'innersquare show backgroundTweak bomb' : 'innersquare hide';
@@ -73,7 +147,10 @@ class Square extends React.Component {
     return (
        <div className={switchSquare}>
 
-      {/* This is not working yet */}
+      {/* This is not working yet -- or it is and is not doing what I thought it would,
+      it's only acting on those that are clicked after it becomes true
+      */}
+
        {  this.state.gameOver === true ?
          (
             <div className={switchClass}>{this.display()}</div>
